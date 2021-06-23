49cc1eabc94189cafd676ebf349609fb593a81d6
Charwekar <Akshay.Charwekar@finastra.com>
2021-06-24 00:10:32 +0530

    Dynamically rendering header menu

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,11 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  headerMenu = [
+    {
+      title: 'Dashboard',
+      subMenu: [
+        "Project Dashboard",
+        "Release Dashboard"
+      ]
+    },
+    {
+      title: 'Project',
+      subMenu: [
+        "Dashboard 4",
+        "Dashboard 5"
+      ]
+    },
+    {
+      title: 'Issues',
+      subMenu: [
+        "Dashboard 4",
+        "Dashboard 5"
+      ]
+    },
+    {
+      title: 'Capture',
+      subMenu: [
+        "Dashboard 4",
+        "Dashboard 5"
+      ]
+    },
+    {
+      title: 'Boards',
+      subMenu: [
+        "Dashboard 4",
+        "Dashboard 5"
+      ]
+    },
+    {
+      title: 'Plans',
+      subMenu: [
+        "Dashboard 4",
+        "Dashboard 5"
+      ]
+    },
+    {
+      title: 'Git',
+      subMenu: [
+        "Dashboard 4",
+        "Dashboard 5"
+      ]
+    }
+    
+  ];
 
   constructor() { }
 
--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from "@angular/core";
+import {MatIconModule} from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
     exports: [
         MatToolbarModule,
+        MatIconModule,
+        MatMenuModule,
     ]
 })
 export class MaterialModule { }
