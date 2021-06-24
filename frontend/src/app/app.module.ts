a267848bae24286a90c2e52cbcc0dc6848f094ee
Charwekar <Akshay.Charwekar@finastra.com>
2021-06-24 10:26:19 +0530

    Added dynamically loading of svg as mat-icon

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HeaderComponent } from './components/header/header.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { HeaderComponent } from './components/header/header.component';
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core";
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { DomSanitizer } from "@angular/platform-browser";
 
 @NgModule({
     exports: [
@@ -12,4 +13,18 @@ import {MatDividerModule} from '@angular/material/divider';
         MatDividerModule,
     ]
 })
-export class MaterialModule { }
+export class MaterialModule {
+    customIcons: Array<[string, string]> = [
+        ["app_switcher", "assets/icons/app_switcher.svg"]
+    ];
+
+    constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+        this.customIcons.forEach(([iconName, iconPath]) => {
+            iconRegistry.addSvgIcon(
+                iconName,
+                sanitizer.bypassSecurityTrustResourceUrl(iconPath),
+            );
+        });
+    }
+
+}
