a2825f018961655d6d97d4ad21f322f06f4986c4
CharwekarAkshay <charwekar12@gmail.com>
2021-06-25 09:52:29 +0530

    Header content complete working on designing

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -1,40 +1,40 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { DomSanitizer } from "@angular/platform-browser";
-
+import { DomSanitizer } from '@angular/platform-browser';
 
 @NgModule({
-    exports: [
-        MatToolbarModule,
-        MatIconModule,
-        MatMenuModule,
-        MatDividerModule,
-        MatButtonModule,
-        MatAutocompleteModule,
-        MatFormFieldModule,
-        MatInputModule,
-    ]
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+  ],
 })
 export class MaterialModule {
-    customIcons: Array<[string, string]> = [
-        ["app_switcher", "assets/icons/app_switcher.svg"],
-        ["export", "assets/icons/export.svg"]
-    ];
+  customIcons: Array<[string, string]> = [
+    ['app_switcher', 'assets/icons/app_switcher.svg'],
+    ['export', 'assets/icons/export.svg'],
+  ];
 
-    constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-        this.customIcons.forEach(([iconName, iconPath]) => {
-            iconRegistry.addSvgIcon(
-                iconName,
-                sanitizer.bypassSecurityTrustResourceUrl(iconPath),
-            );
-        });
-    }
-
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    this.customIcons.forEach(([iconName, iconPath]) => {
+      iconRegistry.addSvgIcon(
+        iconName,
+        sanitizer.bypassSecurityTrustResourceUrl(iconPath)
+      );
+    });
+  }
 }
