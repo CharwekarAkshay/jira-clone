eaff0d4e6a8f91b927c7d81d6f2389fec96c6017
CharwekarAkshay <charwekar12@gmail.com>
2021-07-07 09:31:15 +0530

    Create project component created

--- frontend/src/app/components/create-project/create-project.component.ts
+++ frontend/src/app/components/create-project/create-project.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-project',
   templateUrl: './create-project.component.html',
-  styleUrls: ['./create-project.component.scss']
+  styleUrls: ['./create-project.component.scss'],
+  host: {
+    class: 'h-100',
+  }
 })
 export class CreateProjectComponent implements OnInit {
+  createProjectFormGroup: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.createProjectFormGroup = new FormGroup({
+      projectName: new FormControl(null, [Validators.required]),
+      projectKey: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(5),
+      ]),
+    });
   }
 
+  ngOnInit(): void {}
 }
